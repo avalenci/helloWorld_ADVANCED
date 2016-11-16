03dbe11655b224deaec11809213ef1b7ee3a765f
avalenci <ansmeew@gmail.com>
2016-11-16 14:29:12 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World\nHello World"); //"\n" will make the rest of the String start on a new line
+    }
+}
